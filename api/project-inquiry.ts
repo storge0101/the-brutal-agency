605b10ad568232cd5ee986c818c972442ba12951
storge0101 <psacinabbe@gmail.com>
2026-06-04 16:03:07 +0800

    Add files via upload

--- api/project-inquiry.ts
+++ api/project-inquiry.ts
@@ -1,6 +1,4 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
